e3e8a4d08e25b370f6763d7fc75f421b5e8b2168
Reggie Hsu <reggiehsu111@gmail.com>
2020-04-27 17:14:29 +0800

    all files

---
+++ Top.v
@@ -0,0 +1,14 @@
+module Top(i_rst, i_clk, a, prime, Px, Py, k, kP);
+    input i_rst;
+    input i_clk;
+    input [3:0] a;
+    input [3:0] prime;
+    input [3:0] Px;
+    input [3:0] Py;
+    input [3:0] k;
+    
+    output [3:0] kP;
+
+    
+
+endmodule
