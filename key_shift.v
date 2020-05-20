0099bd3c21f058ed58950ec23dc2a3f369914c82
Ting <ting@Tingde-MacBook-Pro.local>
2020-05-21 00:22:21 +0800

    Modified key shifter

--- key_shift.v
+++ key_shift.v
@@ -1,10 +1,45 @@
-module key_shift();
+module key_shift(
+	i_clk,
+	i_rst,
+	k,
+	done_from_control,
+	k_out);
 	localparam SH_NUM = 1;
 	localparam SIZE = 32;
+	localparam OUT_SIZE = 1;
 
 	input i_clk, i_rst; 
 	input [SIZE - 1 : 0] k;
+	input done_from_control;
 
 
-	output  
+	output k_out;
+
+	reg k_out, k_out_n;
+	reg [6:0] i, i_n;
+
+	always @(*) begin
+		k_out = k[i];
+		if (done_from_control == 1) begin
+			i_n = i + 1;
+		end
+		else begin
+			i_n = i;
+		end
+	end
+	always@ (posedge i_clk or posedge i_rst) begin
+		if (i_rst) begin
+			i = 0;
+		end
+		else begin
+			i <= i_n;
+		end
+	end
+
+
+
+
+
+
+
 endmodule 
