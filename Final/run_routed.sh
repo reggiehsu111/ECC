b15ee15e4aba789b95bc8770062438a4e0bc0d87
Reggie Hsu <reggiehsu111@gmail.com>
2020-06-30 16:43:16 +0800

    Fuck

---
+++ Final/run_routed.sh
@@ -0,0 +1,1 @@
+ncverilog ECC_Lookup_tb.v CHIP_APR.v -v fsa0m_a_generic_core_21.lib.src fsa0m_a_t33_generic_io_21.lib.src +define+SDF +access+rw
--- Top.v
+++ Top.v
@@ -9,10 +9,10 @@ module Top(rst, clk, ready, valid, a, prime, Px, Py, k, kP);
     output ready;
     output [3:0] kP;
 
-    reg [31:0] prime_whole, px_whole, py_whole, a_whole, k_whole;
-    reg [31:0] prime_whole_nxt, px_whole_nxt, py_whole_nxt, a_whole_nxt, k_whole_nxt;
+    reg [31:0] prime_whole, px_whole, py_whole, a_whole, k_whole, output1, output2;
+    reg [31:0] prime_whole_nxt, px_whole_nxt, py_whole_nxt, a_whole_nxt, k_whole_nxt, output1_nxt, output2_nxt; 
     reg [2:0] counter, counter_nxt;
-    reg state, state_nxt;
+    reg [1:0] state, state_nxt;
 
     wire GFAU_done, Keyshift_done, PartKey, done_keyshift, done_control;
     wire prime_in, px_in, py_in, a_in ,k_in;
@@ -26,78 +26,65 @@ module Top(rst, clk, ready, valid, a, prime, Px, Py, k, kP);
     assign k_in = k_whole;
 
     Control ctrl(.i_clk(clk), .i_rst(rst), .GFAU_done(GFAU_done), .Keyshift_done(Keyshift_done),
-    	.PartKey(PartKey), .GFAU_result(GFAU_result), .Px_mont(Px_mont), .Py_mont(Py_mont), .operation_select(op_s), .done_keyshift(done_keyshift), .done_control(done_control), .raw1(px_in), .raw2(py_in), .raw_prime(prime_in));
+    	.PartKey(PartKey), .GFAU_result(GFAU_result), .Px_mont(Px_mont), .Py_mont(Py_mont), .operation_select(op_s), .done_keyshift(done_keyshift), .done_control(done_control), .raw1(px_in), .raw2(py_in), .raw_prime(prime_in), .output1(output1), .output2(output2));
 
     GFAU gfau(.i_clk(clk), .i_rst(rst), .in_0(px_in), .in_1(py_in), .prime(prime_in), .operation_select(op_s), .done_from_control(done_control), .result(GFAU_result), .done_to_control(GFAU_done));
 
 
+    always @(*) begin
+    // 0: IDLE, 1: Input, 2: Output, 3: X
+    	if(valid) begin
+    		// switch to input state when valid
+    		state_nxt = 1;
+    	else if (counter == 7 and state == 1) begin
+    		// switch to idle state when input is done
+    		state_nxt = 0;
+    	end
+    	else if (counter == 15 and state == 2) begin
+    		// switch to idle state when output is done
+    		state_nxt = 0;
+    	end
+    	else if () begin
+    		// switch to output state when control done
+    		state_nxt = 2;
+    	end
+    	else begin
+    		// else remain last state
+    		state_nxt = state;
+    	end
+    end
 
 
     always @(*) begin
-    	// state = 1 after valid signal, until counter = 7
-    	if(valid) begin
-    		state_nxt = 1;
-    		// Load 
-    		if (state) begin
-	    		prime_whole_nxt = (prime_whole << 4) + prime;
-	    		px_whole_nxt = (px_whole << 4) + Px;
-	    		py_whole_nxt = (py_whole << 4) + Py;
-	    		a_whole_nxt = (a_whole << 4) + a;
-	    		k_whole_nxt = (k_whole << 4) + k;
-	    		counter_nxt = counter + 1;
-    		end
-    		else begin
-    			prime_whole_nxt = prime_whole;
+    	case(state) 
+    		// 0: IDLE, 1: Read in, 2: Output, 3: X
+			0: begin
+				prime_whole_nxt = prime_whole;
     			px_whole_nxt = px_whole;
     			py_whole_nxt = py_whole;
     			a_whole_nxt = a_whole;
     			k_whole_nxt = k_whole;
+    			output1_nxt = output1;
+	    		output2_nxt = output2;
     			counter_nxt = 0;
-    		end
-    	end
-
-    	
-    	else if (counter == 7) begin 
-    		state_nxt = 0;
-    		if (state) begin
-	    		prime_whole_nxt = (prime_whole << 4) + prime;
+			end
+			1: begin
+				prime_whole_nxt = (prime_whole << 4) + prime;
 	    		px_whole_nxt = (px_whole << 4) + Px;
 	    		py_whole_nxt = (py_whole << 4) + Py;
 	    		a_whole_nxt = (a_whole << 4) + a;
 	    		k_whole_nxt = (k_whole << 4) + k;
+	    		output1_nxt = output1;
+	    		output2_nxt = output2;
 	    		counter_nxt = counter + 1;
-    		end
-    		else begin
-    			prime_whole_nxt = prime_whole;
-    			px_whole_nxt = px_whole;
-    			py_whole_nxt = py_whole;
-    			a_whole_nxt = a_whole;
-    			k_whole_nxt = k_whole;
-    			counter_nxt = 0;
-    		end
-    	end
-    	else begin
-    		state_nxt = state;
-    		if (state) begin
-	    		prime_whole_nxt = (prime_whole << 4) + prime;
-	    		px_whole_nxt = (px_whole << 4) + Px;
-	    		py_whole_nxt = (py_whole << 4) + Py;
-	    		a_whole_nxt = (a_whole << 4) + a;
-	    		k_whole_nxt = (k_whole << 4) + k;
-	    		counter_nxt = counter + 1;
-    		end
-    		else begin
-    			prime_whole_nxt = prime_whole;
-    			px_whole_nxt = px_whole;
-    			py_whole_nxt = py_whole;
-    			a_whole_nxt = a_whole;
-    			k_whole_nxt = k_whole;
-    			counter_nxt = 0;
-    		end
-    	end
-
-    	 
-    	
+			end
+			2: begin
+				output1_nxt = 
+				counter_nxt = counter + 1;
+			end
+			default; begin
+				
+			end
     end
 
     always @(posedge clk or posedge rst) begin
@@ -109,6 +96,8 @@ module Top(rst, clk, ready, valid, a, prime, Px, Py, k, kP);
     		k_whole <= 0;
     		counter <= 0;
     		state <= 0;
+    		output1 <= 0;
+    		output2 <= 0;
     	end
     	else begin
     		prime_whole <= prime_whole_nxt;
@@ -118,6 +107,8 @@ module Top(rst, clk, ready, valid, a, prime, Px, Py, k, kP);
     		k_whole <= k_whole_nxt;
     		counter <= counter_nxt;
     		state <= state_nxt;
+    		output1 <= output1_nxt;
+    		output2 <= output2_nxt;
     	end
     end
 
